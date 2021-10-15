a68e8f0498983da2eb06e3cbc35e69cc4e9dc3d6
shubham <shubhamgupta20041998@gmail.com>
2021-10-15 09:58:33 +0530

    Update AboutMe.js

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,18 +8,22 @@ function Aboutme()
         <h1 className="text-info font-weight-light">
           About <span className="text-dark">Me</span>
         </h1>
-        <div className="lead">I'm A Full Stack Developer</div>
+        <div className="lead leades">I'm A Full Stack Developer</div>
         <div className="row py-3">
           <div className="col-12 col-md-6">
             <h4 className="text-dark">What I can do?</h4>
-            <p className="text-justify py-3">
+            <p className="text-justify extraclass py-3">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
               voluptatibus in quae perspiciatis quaerat maiores suscipit
               deleniti accusantium! Rem suscipit maiores quos doloremque ipsa
               laboriosam unde perspiciatis voluptate natus consectetur.
+                deleniti accusantium! Rem suscipit maiores quos doloremque ipsa
+              laboriosam unde perspiciatis voluptate natus consectetur.
+        
+        
             </p>
           </div>
-          <div className="col-12 col-md-6">
+          <div className="col-12 col-md-5">
             <h4 className="text-dark">What I am doing currently?</h4>
             <p className="text-justify py-3">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
